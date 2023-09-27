a9f3b58322255bc0208aaa3ea85bdc2a61b323fd
Duy Huynh <duy.huynh@saleshood.com>
2023-09-27 15:20:23 +0700

    feat: sort items before insert

--- app/services/news_service.rb
+++ app/services/news_service.rb
@@ -1,13 +1,14 @@
 class NewsService
-  def insert_items
+  def store_items
     items = data_service.get_news
+    items.sort_by! {|item| item[:published_at]}
     topic_name_hash = Topic.name_hash
     news_topics_records = []
 
     items.each do |item|
       news = News.create item.except(:topics)
       item[:topics].each do |topic|
-        news_topics_records << {news_id: news.id, topics_id: topic_name_hash[topic["topic"]], relevance_score: topic["relevance_score"]}
+        news_topics_records << {news_id: news.id, topic_id: topic_name_hash[topic["topic"]], relevance_score: topic["relevance_score"]}
       end
     end
     NewsTopics.insert_all news_topics_records
